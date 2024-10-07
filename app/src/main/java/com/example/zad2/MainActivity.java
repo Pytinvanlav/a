860e74945bdd24f9ec36c20e8f2660f4c4a35643
Ekaterina <masukovaekaterina97@yandex.com>
2024-10-07 20:09:15 +0300

    Сделала кнопку

---
+++ app/src/main/java/com/example/zad2/MainActivity.java
@@ -0,0 +1,39 @@
+package com.example.zad2;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView helloworld;
+    private ImageButton startButton;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.starbutton), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        helloworld = (TextView) findViewById(R.id.helloworld);
+        startButton = (ImageButton) findViewById(R.id.startButton);
+    }
+
+    public void onStartButtonClick(View view) {
+        startButton.setImageResource(R.drawable.android);
+        helloworld.setVisibility(View.VISIBLE);
+        startButton.setClickable(false);
+    }
+}
